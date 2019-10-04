feed8dc5e080fd0e2f3319b544ebaa46f0a29197
Xuan Hieu <12824658@student.uts.edu.au>
2019-10-04 22:24:53 +1000

    Added points

---
+++ Assets/Scripts/TurningPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningPoint : MonoBehaviour
+{
+    public TurningPoint[] nextPoints;
+    public Vector2[] vectToNextPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
